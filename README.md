958f4d349e2d9950f61d3522ca5d887c0788f373
EyadRealHim <eyad.realhim@gmail.com>
2024-03-19 20:48:31 +0300

    Adding cli (currently inactive)

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ Visual Tilawa is a program that makes it easy to generate videos of Quran recita
 
 ## Features
 
--   **Synchronized Subtitles**:subtitles that show the verses being read.
--   **Synchronized Translation**: synchronizes translations with each verse being recited.
--   **Multilingual Support**: Choose from many languages for the translation.
+- **Synchronized Subtitles**: subtitles that show the verses being read.
+- **Synchronized Translation**: synchronizes translations with each verse being recited.
+- **Multilingual Support**: Choose from many languages for the translation.
+
+## Getting Started
+
+To install dependencies, execute:
+
+```bash
+pip install -r requirements.txt
+```
+
+To explore the command-line interface, run:
+
+```bash
+python -m src.main --help
+```
--- src/main.py
+++ src/main.py
@@ -1,10 +1,66 @@
-from verse import VerseKey, extract_clips, verse_info
+from typing import Tuple
+
+import click
+
+from .verse.types import VerseKey
 
 
+class SizeParam(click.ParamType):
+    name="Size"
+
+    def convert(self, value: str, _a, _b):
+        if isinstance(value, tuple):
+            return value
+        
+        width, height = 0, 0
+        try:
+            width, height = map(int, value.split('x'))
+        except ValueError:
+            raise click.BadParameter("Size must be specified in the format 'WidthxHeight'.")
+        
+        if width <= 0 or height <= 0:
+            raise click.BadArgumentUsage("Width and height must be positive integers.")
+        return width, height
+
+class VerseKeyParam(click.ParamType):
+    name="VerseKey"
+
+    def convert(self, value: str, _a, _b):
+        if isinstance(value, tuple) or isinstance(value, VerseKey):
+            return value
+        
+        chapter_id, verse_id = 0, 0
+        
+        try:
+            chapter_id, verse_id = map(int, value.split(':'))
+        except ValueError:
+            raise click.BadParameter("verse key must be specified in the format 'chapter_id:verse_id'.")
+        
+        if chapter_id <= 0 or verse_id <= 0:
+            raise click.BadArgumentUsage("chapter_id and verse_id must be positive integers.")
+
+        return VerseKey(chapter_id=chapter_id, verse_id=verse_id)
+
+@click.command()
+@click.option("--verse_key", default="1:1", help="which verse to render", prompt="Enter the verse key", type=VerseKeyParam())
+@click.option("--dist", default="dist", help="where to store the videos", prompt="Enter the output directory", type=str)
+@click.option("--fps", default=30, help="Frames per second of a video.", prompt="Enter the frames per second", type=int)
+@click.option("--size", default="540x1080", help="frame size", prompt="Enter the frame size", type=SizeParam())
+def App(verse_key: VerseKey, dist: str, fps: int, size: Tuple[int, int]):
+    """program that makes it easy to generate videos of Quran recitation."""
+    line_text = "-" * 10
+
+    click.echo("")
+    click.echo(f"{line_text}Summary{line_text}")
+    click.echo(f"key: {verse_key}\tdist: {dist}")
+    click.echo(f"size: {size}\tfps: {fps}")
+    click.echo("")
+
+    if not click.confirm("Do you want to proceed?", default=True):
+        return
+    
+    click.echo("Generating...")
+    click.echo("Coming soon!")
 
 if __name__ == "__main__":
-    verse = verse_info(VerseKey(chapter_id=1, verse_id=1))
-
-    print(
-        extract_clips(verse)
-    )
+    App()
