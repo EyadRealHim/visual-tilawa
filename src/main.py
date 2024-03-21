e01aa7e449ad3e3ba54fc53d285d65824686d5b9
savege6666 <savege6666@gmail.com>
2024-03-21 14:20:28 +0000

    renaming 'size' to 'resolution'

--- src/main.py
+++ src/main.py
@@ -5,11 +5,19 @@ import click
 from .verse.types import VerseKey
 
 
+class VideoResolution():
+    def __init__(self, width: int, height: int):
+        self.height = height
+        self.width = width
+
+    def __str__(self) -> str:
+        return f"{self.width}x{self.height}"
+
 class SizeParam(click.ParamType):
     name="Size"
 
     def convert(self, value: str, _a, _b):
-        if isinstance(value, tuple):
+        if isinstance(value, tuple) or isinstance(value, VideoResolution):
             return value
         
         width, height = 0, 0
@@ -20,7 +28,7 @@ class SizeParam(click.ParamType):
         
         if width <= 0 or height <= 0:
             raise click.BadArgumentUsage("Width and height must be positive integers.")
-        return width, height
+        return VideoResolution(width=width, height=height)
 
 class VerseKeyParam(click.ParamType):
     name="VerseKey"
@@ -45,15 +53,15 @@ class VerseKeyParam(click.ParamType):
 @click.option("--verse_key", default="1:1", help="which verse to render", prompt="Enter the verse key", type=VerseKeyParam())
 @click.option("--dist", default="dist", help="where to store the videos", prompt="Enter the output directory", type=str)
 @click.option("--fps", default=30, help="Frames per second of a video.", prompt="Enter the frames per second", type=int)
-@click.option("--size", default="540x1080", help="frame size", prompt="Enter the frame size", type=SizeParam())
-def App(verse_key: VerseKey, dist: str, fps: int, size: Tuple[int, int]):
+@click.option("--resolution", default="540x1080", help="video resolution", prompt="Enter the video resolution", type=SizeParam())
+def App(verse_key: VerseKey, dist: str, fps: int, resolution: VideoResolution):
     """program that makes it easy to generate videos of Quran recitation."""
     line_text = "-" * 10
 
     click.echo("")
     click.echo(f"{line_text}Summary{line_text}")
     click.echo(f"key: {verse_key}\tdist: {dist}")
-    click.echo(f"size: {size}\tfps: {fps}")
+    click.echo(f"size: {resolution}\tfps: {fps}")
     click.echo("")
 
     if not click.confirm("Do you want to proceed?", default=True):
