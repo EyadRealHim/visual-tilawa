c079901dd867fa3c7137df4d88f2f6477b615d49
EyadRealHim <134924482+EyadRealHim@users.noreply.github.com>
2025-08-27 22:43:56 +0300

    Update utilities.py

--- src/verse/utilities.py
+++ src/verse/utilities.py
@@ -1,11 +1,28 @@
 from .types import ReciterName, Reciter
 
 
+_RECITERS = {
+    "AbdulBaset AbdulSamad": 1,
+    "Abdur-Rahman as-Sudais": 2,
+    "Abu Bakr al-Shatri": 3,
+    "Hani ar-Rifai": 4,
+    "Mahmoud Khalil Al-Husary": 5,
+    "Mishari Rashid al-`Afasy": 6,
+    "Mohamed Siddiq al-Minshawi": 7,
+    "Sa'ud ash-Shuraim": 8,
+    "Khalifah Al Tunaiji": 11,
+    "Sa'ad al-Ghamdi": 12,
+    "Yasser Ad Dussary": 20,
+    "Ahmed ibn Ali al-Ajmy": 22,
+    "Abdullah Ali Jabir": 23,
+    "Bandar Baleela": 24,
+    "Maher al-Muaiqly": 25,
+    "Abdullah Hamad Abu Sharida": 26,
+}
+
+
 def get_reciter_config(name: ReciterName) -> Reciter:
-    """
-    names: Sa'ud ash-Shuraim, Mahmoud Khalil Al-Husary
-    """
-    if name == "Sa'ud ash-Shuraim":
-        return Reciter(name=name, id=4, silence_threshold=8)
-    if name == "Mahmoud Khalil Al-Husary":
-        return Reciter(name=name, id=5, silence_threshold=8)
+    try:
+        return Reciter(name=name, id=_RECITERS[name], silence_threshold=8)
+    except KeyError:
+        raise ValueError(f"Unsupported reciter: {name}")
